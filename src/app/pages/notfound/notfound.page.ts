d6798c081ef728ca514816bde1118137a89c3323
FernandaArrano <fernandamarcela714@gmail.com>
2022-09-28 22:00:21 -0300

    implementacion 404 completa

--- src/app/pages/notfound/notfound.page.ts
+++ src/app/pages/notfound/notfound.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController, createAnimation } from '@ionic/angular';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundPage implements OnInit {
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) {
+    this.animacion();
+   }
 
   ngOnInit() {
+  }
+
+  animacion() {
+    const squareA = createAnimation()
+      .addElement(document.querySelector('#notfound'))
+      .duration(5000)
+      .keyframes([
+        { offset: 0, transform: 'scale(1))', opacity: '0.5' },
+        { offset: 0.5, transform: 'scale(0.8)', opacity: '1' },
+        { offset: 1, transform: 'scale(1)', opacity: '0.5' }
+      ]);
+
+    const parent = createAnimation()
+      .duration(2000)
+      .iterations(Infinity)
+      .addAnimation([squareA]);
+
+    parent.play();
   }
 
 }
